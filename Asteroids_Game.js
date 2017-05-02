233403baff9739fa386ea151fd1f129f4519a462
drmurillo <hotpuffy1@gmail.com>
2017-05-02 15:37:41 -0500

    Added lazer firing

--- Asteroids_Game.js
+++ Asteroids_Game.js
@@ -1,6 +1,9 @@
 var ship;
+
 var asteroids = [];
-var asteroidCount = 20;
+var asteroidCount = 10;
+
+var lazers = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,27 +15,33 @@ function setup() {
 
 function draw() {
   background(0);
-  ship.show();
-  ship.move();
-  ship.edges();
+
   for (i = 0; i < asteroids.length; i++) {
     asteroids[i].show();
     asteroids[i].move();
     asteroids[i].edges();
   }
+  for (i = 0; i < lazers.length; i++) {
+    lazers[i].show();
+    lazers[i].move();
+    //lazers[i].edges();
+  }
+  ship.show();
+  ship.move();
+  ship.edges();
+
   if (keyIsDown(LEFT_ARROW)) {
     ship.turn(-0.08);
   } else if (keyIsDown(RIGHT_ARROW)) {
     ship.turn(0.08);
   }
-
   if (keyIsDown(UP_ARROW)) {
     ship.boost();
   }
 }
 
-/*function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    ship.boost();
+function keyPressed() {
+  if (keyCode == 32) {
+    lazers.push(new Lazer(ship.pos, ship.heading, ship.front));
   }
-} */
+}
