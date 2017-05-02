966a65fbd06384f9b1b45477b189107a032ab245
drmurillo <hotpuffy1@gmail.com>
2017-05-02 12:07:31 -0500

    Added screen wrapping, dynamic generated asteroids

--- asteroids.js
+++ asteroids.js
@@ -1,42 +1,51 @@
 function Asteroid() {
   this.pos = createVector(random(width), random(height));
-  this.radius = 20;
-  this.vel = createVector(random(-1, 1), random(-1, 1));
+  //Size of Asteroid
+  this.r = random(15, 75);
 
+  this.vel = createVector(random(-1, 1), random(-1, 1));
+  this.total = floor(random(5, 15));
+
+  this.mutate = [];
+  for (var i = 0; i < this.total; i++) {
+    this.mutate[i] = random(-12, 12);
+  }
   this.show = function() {
     push();
     stroke(255);
     noFill();
     translate(this.pos.x, this.pos.y);
     beginShape();
-    //vertex(this.pos.x, this.pos.y);
-    vertex(this.pos.x + 10, this.pos.y - 10);
-    vertex(this.pos.x + 20, this.pos.y - 15);
-    endShape();
-    ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
+    for (var i = 0; i < this.total; i++) {
+      var angle = map(i, 0, this.total, 0, TWO_PI);
+      var r = this.r + this.mutate[i];
+      var x = r * cos(angle);
+      var y = r * sin(angle);
+      vertex(x, y);
+    }
+    endShape(CLOSE);
     pop();
   };
 
-  this.shapeChanger = function() {
-    // mutate verex x and y position within a given radius.
-    var mutateX = random(5, 20);
-    var mutateY = random(5, 20);
-    return mutateX;
-    return mutateY;
-  };
   this.move = function() {
     this.pos.add(this.vel);
   };
 
   this.edges = function() {
-    if (this.pos.x > width) {
-      this.pos.x = width - width;
-    } else if (this.pos.x < 0) {
-      this.pos.x = width;
-    } else if (this.pos.y > height) {
-      this.pos.y = height - height;
-    } else if (this.pos.y < 0) {
-      this.pos.y = height;
+    var left = width - width;
+    var right = width;
+    var top = height - height;
+    var bottom = height;
+
+    if (this.pos.x - this.r > right) {
+      this.pos.x = left - this.r;
+    } else if (this.pos.x + this.r < left) {
+      this.pos.x = right + this.r;
+    }
+    if (this.pos.y - this.r > bottom) {
+      this.pos.y = top - this.r;
+    } else if (this.pos.y < top - this.r) {
+      this.pos.y = bottom + this.r;
     }
   };
 }
