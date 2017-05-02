a6cf010ce3e8813b72c17fa041c8c6a69d4373f5
drmurillo <hotpuffy1@gmail.com>
2017-05-02 15:38:35 -0500

    Adjusted starting heading

--- ship.js
+++ ship.js
@@ -1,8 +1,9 @@
 function Ship() {
   this.pos = createVector(width / 2, height / 2);
   this.r = 20;
-  this.heading = 0;
+  this.heading = -1.5;
   this.vel = createVector(0, 0);
+  this.front = createVector(0, -this.r);
 
   this.show = function() {
     //Adjusts the width of the ship. Higher value = skinnier ship
@@ -11,10 +12,9 @@ function Ship() {
     translate(this.pos.x, this.pos.y);
     rotate(this.heading + PI / 2);
     stroke(255);
-    noFill();
     //Ship center is x: 0, y: 0
     //Top to Bottom Left
-    line(0, -this.r, -this.r + widthAdj, this.r);
+    line(this.front.x, this.front.y, -this.r + widthAdj, this.r);
     //Bottom Right to Top
     line(this.r - widthAdj, this.r, 0, -this.r);
     //Bottom left to Center
@@ -33,9 +33,11 @@ function Ship() {
   this.turn = function(angle) {
     this.heading = this.heading + angle;
   };
+
   this.move = function() {
     this.pos.add(this.vel);
   };
+
   this.edges = function() {
     var left = width - width;
     var right = width;
