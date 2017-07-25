87c27075a5083f10476c32f694ecfe7d1ffe085e
drmurillo <hotpuffy1@gmail.com>
2017-07-25 16:21:50 -0500

    Fixed collision on bricks, background color
    to do: fix ball bounce on paddle

--- brick_game.js
+++ brick_game.js
@@ -11,7 +11,9 @@ var paddlePicLoaded = false;
 
 var bricks;
 var bricksPic = document.createElement('img');
-var paddlePicLoaded = false;
+var bricksPicLoaded = false;
+var brickW = 100;
+var brickH = 20;
 
 var brickCount;
 var bricksLeft;
@@ -34,7 +36,7 @@ window.onload = function() {
     bricksPicLoaded = true;
   };
 
-  ball = new Ball(canvas.width / 2, canvas.height / 2, ballPic);
+  ball = new Ball(canvas.width / 2, canvas.width / 2, ballPic);
   paddle = new Paddle(paddlePic);
   grid = new BrickGrid(8, 8);
   grid.create();
@@ -44,18 +46,28 @@ window.onload = function() {
 };
 
 function draw() {
-  //Sky blue color: 8ce6ff
-  background('black');
+  background('#8ce6ff');
   //computerMovement(paddle, ball);
   //Debugging
   //MouseX/Y displayed at cursor
   //text(mouseX + ',' + mouseY, mouseX + 10, mouseY, 'yellow');
-  text(Math.floor(mouseX / 100) + ',' + Math.floor(mouseY / 20), mouseX + 10, mouseY, 'yellow');
+  //var mouseBrickCol = Math.floor(mouseX / brickW);
+  //var mouseBrickRow = Math.floor(mouseY / brickH);
+  //var brickIndexUnderMouse = ColrowToArrayIndex(grid, mouseBrickCol, mouseBrickRow);
+    //text(mouseBrickCol + ',' + mouseBrickRow + ':' + brickIndexUnderMouse, mouseX, mouseY, 'yellow');
   ball.show();
   ball.update();
   paddle.show();
   for (i = 0; i < grid.bricks.length; i++) {
-    grid.bricks[i].show();
+    if (grid.bricks[i].display == true) {
+      grid.bricks[i].show();
+    }
+  }
+  for (i = 0; i < grid.bricks.length; i++) {
+    if (ball.collidesWith(grid.bricks[i])) {
+      grid.bricks[i].display = false;
+      bricksLeft--;
+    }
   }
 }
 
@@ -75,22 +87,33 @@ function BrickGrid(cols, rows) {
   this.brickRows = rows;
 
   this.create = function() {
-    for (var x = 0; x < this.brickCols; x++) {
-      for (var y = 3; y < this.brickRows; y++) {
-        this.bricks.push(new Brick(x, y, bricksPic));
+    var extraBricks = 0;
+    for (var x = 0; x < this.brickRows + 1; x++) {
+      for (var y = 0; y < this.brickCols; y++) {
+        this.bricks.push(new Brick(y, x, bricksPic));
       }
     }
-    bricksLeft = grid.bricks.length;
+    for (var i = 0; i < 3 * this.brickCols; i++) {
+      this.bricks[i].display = false;
+    }
+    for (var i = this.brickRows * this.brickCols; i < grid.bricks.length; i++) {
+      this.bricks[i].display = false;
+      extraBricks++;
+    }
+    bricksLeft = grid.bricks.length - 3 * this.brickCols - extraBricks;
   };
 }
 
 function Brick(x, y, img) {
-  this.width = 100;
-  this.height = 20;
+  this.width = brickW;
+  this.height = brickH;
   this.x = x * this.width;
   this.y = y * this.height;
 
-  this.arrayIndex = (x + y) * x;
+  this.display = true;
+  this.col = Math.floor(this.x / this.width);
+  this.row = Math.floor(this.y / this.height);
+  this.arrayIndex = ColrowToArrayIndex(grid, this.col, this.row);
   this.img = img;
   this.img.src = 'http://res.cloudinary.com/snugglepigs/image/upload/v1500320826/brick_l3hv0e.png';
 
@@ -153,8 +176,7 @@ function Ball(x, y, img) {
     /*if (mouseX && mouseY) {
       this.x = mouseX;
       this.y = mouseY;
-    }*/
-    fill('red');
+    } */
     if (ballPicLoaded) {
       canvasContext.drawImage(this.img,
         this.x - this.img.width / 2, this.y - this.img.height / 2);
@@ -187,12 +209,41 @@ function Ball(x, y, img) {
     }
   };
 
-  this.collidesWith = function(grid) {
-    //text(Math.floor(mouseX / 100) + ',' + Math.floor(mouseY / 20), mouseX + 10, mouseY, 'yellow');
-    for (i = 0; i < grid.bricks.length; i++) {
-      if (Math.floor(mouseY / 20) == Math.floor(grid.bricks[i].y / 20 &&
-      Math.floor(mouseX / 100) == Math.floor(grid.bricks[i].x / 100))) {
-        //console.log('true');
+  this.collidesWith = function(brick) {
+    var ballCol = Math.floor(this.x / brickW);
+    var ballRow = Math.floor(this.y / brickH);
+    var ballIndex = ColrowToArrayIndex(grid, ballCol, ballRow);
+    if (ballCol >= 0 && ballCol < grid.brickCols &&
+        ballRow >= 0 && ballRow < grid.brickRows) {
+      var xPrevious = this.x - this.xSpeed;
+      var yPrevious = this.y - this.ySpeed;
+      var previousBallCol = Math.floor(xPrevious / brickW);
+      var previousBallRow = Math.floor(yPrevious / brickH);
+      if (ballIndex == brick.arrayIndex && brick.display == true) {
+        var bothTestsFailed = true;
+        if (previousBallCol != ballCol) {
+          var adjBrickSide = ColrowToArrayIndex(grid, previousBallCol, ballRow);
+          if (adjBrickSide <= grid.brickRows) {
+            if (grid.bricks[adjBrickSide].display == false) {
+              this.xSpeed *= -1;
+              bothTestsFailed = false;
+            }
+          }
+        }
+        if (previousBallRow != ballRow) {
+          var adjBrickTopBot = ColrowToArrayIndex(grid, ballCol, previousBallRow);
+          if (grid.bricks[adjBrickTopBot].display == false) {
+            this.ySpeed *= -1;
+            bothTestsFailed = false;
+          }
+        }
+        if (bothTestsFailed) { //prevents ball from going through 2 adjacent bricks
+          this.xSpeed *= -1;
+          this.ySpeed *= -1;
+        }
+        return true;
+      } else {
+        return false;
       }
     }
   };
@@ -220,7 +271,7 @@ function Ball(x, y, img) {
     if (this.paddleCollision(paddle)) {
       this.ySpeed *= -1;
       if (bricksLeft == 0) {
-        this.reset();
+        grid.bricks = [];
         grid.create();
       }
     }
@@ -229,9 +280,20 @@ function Ball(x, y, img) {
 }
 
 //HELPER FUNCTIONS
-function ColrowToArrayIndex(grid, col, row) {
-  return row + grid.brickCols * col;
+function isBrickAtColRow(col, row) {
+  if (col >= 0 && col < grid.brickCols &&
+    row >= 0 && row < grid.brickRows) {
+    var brickIndexUnderCoord = ColrowToArrayIndex(grid, col, row);
+    return brickGrid[brickIndexUnder];
+  } else {
+    return false;
+  }
 }
+
+function ColrowToArrayIndex(grid, col, row) {
+  return col + grid.brickCols * row;
+}
+
 function text(string, x, y, color) {
   canvasContext.fillStyle = color;
   canvasContext.fillText(string, x, y);
